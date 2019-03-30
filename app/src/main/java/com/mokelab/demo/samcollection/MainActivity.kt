12a419414417b3e5746d4895a5092576ee81966c
hrk fkm <fkm@mokelab.com>
2019-03-30 22:50:09 +0900

    Setup navigation

--- app/src/main/java/com/mokelab/demo/samcollection/MainActivity.kt
+++ app/src/main/java/com/mokelab/demo/samcollection/MainActivity.kt
@@ -2,11 +2,21 @@ package com.mokelab.demo.samcollection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navController = Navigation.findNavController(this, R.id.container)
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return Navigation.findNavController(this, R.id.container).navigateUp()
+    }
+
 }
