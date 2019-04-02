5bbdddf3756b43970de25993e5277071773c1ba0
hrk fkm <fkm@mokelab.com>
2019-04-02 20:08:49 +0900

    Add room top page

--- app/src/main/java/com/mokelab/demo/samcollection/jetpack/arch/room/JetpackRoomTopFragment.kt
+++ app/src/main/java/com/mokelab/demo/samcollection/jetpack/arch/room/JetpackRoomTopFragment.kt
@@ -1,17 +1,14 @@
-package com.mokelab.demo.samcollection.jetpack.arch
+package com.mokelab.demo.samcollection.jetpack.arch.room
 
 import androidx.navigation.Navigation
 import com.mokelab.demo.samcollection.MenuListFragment
 import com.mokelab.demo.samcollection.R
 
-class JetpackArchTopFragment : MenuListFragment() {
-    override fun onCreateMenuLabel() = resources.getStringArray(R.array.jetpack_arch).toList()
+class JetpackRoomTopFragment : MenuListFragment() {
+    override fun onCreateMenuLabel() = resources.getStringArray(R.array.jetpack_room).toList()
 
     override fun onMenuItemClicked(position: Int) = when (position) {
         0 -> this.navigate(R.id.action_jetpackArch_to_jetpackDatabinding)
-        1 -> this.navigate(R.id.action_jetpackArchTop_to_jetpackLiveData)
-        2 -> this.navigate(R.id.action_jetpackArch_to_ViewModel)
-        3 -> this.navigate(R.id.action_jetpackArch_to_Room)
         else -> println("nop")
     }
 
