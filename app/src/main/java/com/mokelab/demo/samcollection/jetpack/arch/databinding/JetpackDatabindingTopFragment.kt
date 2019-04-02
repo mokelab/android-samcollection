f2e25e2e7e4eca0cec5025c2f7c16111539b50aa
hrk fkm <fkm@mokelab.com>
2019-04-02 09:58:10 +0900

    Add databinding top page

--- app/src/main/java/com/mokelab/demo/samcollection/jetpack/arch/databinding/JetpackDatabindingTopFragment.kt
+++ app/src/main/java/com/mokelab/demo/samcollection/jetpack/arch/databinding/JetpackDatabindingTopFragment.kt
@@ -1,15 +1,14 @@
-package com.mokelab.demo.samcollection.jetpack.arch
+package com.mokelab.demo.samcollection.jetpack.arch.databinding
 
 import androidx.navigation.Navigation
 import com.mokelab.demo.samcollection.MenuListFragment
 import com.mokelab.demo.samcollection.R
 
-class JetpackArchTopFragment : MenuListFragment() {
-    override fun onCreateMenuLabel() = resources.getStringArray(R.array.jetpack_arch).toList()
+class JetpackDatabindingTopFragment : MenuListFragment() {
+    override fun onCreateMenuLabel() = resources.getStringArray(R.array.jetpack_databinding).toList()
 
     override fun onMenuItemClicked(position: Int) = when (position) {
-        0 -> this.navigate(R.id.action_jetpackArch_to_jetpackDatabinding)
-        1 -> this.navigate(R.id.action_jetpackArchTop_to_jetpackLiveData)
+        0 -> this.navigate(R.id.action_jetpackArchTop_to_jetpackLiveData)
         else -> println("nop")
     }
 
