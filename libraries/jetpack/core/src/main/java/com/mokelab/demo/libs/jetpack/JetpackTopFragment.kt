6d2613773dfb89be05e1ceaafed49dc9385572fa
Hirokazu Fukami <fkm@mokelab.com>
2020-07-28 20:17:41 +0900

    Add jetpack core module

---
+++ libraries/jetpack/core/src/main/java/com/mokelab/demo/libs/jetpack/JetpackTopFragment.kt
@@ -0,0 +1,19 @@
+package com.mokelab.demo.libs.jetpack
+
+import androidx.fragment.app.Fragment
+
+class JetpackTopFragment : Fragment(R.layout.jetpack_top_fragment) {
+/*
+    override fun onCreateMenuLabel() = resources.getStringArray(R.array.jetpack_top).toList()
+
+    override fun onMenuItemClicked(position: Int) = when (position) {
+        1 -> this.navigate(R.id.action_jetpackTop_to_jetpackArch)
+        3 -> this.navigate(R.id.action_jetpackTop_to_jetpackUI)
+        else -> println("nop")
+    }
+
+    private fun navigate(action: Int) {
+        Navigation.findNavController(view!!).navigate(action)
+    }
+ */
+}
